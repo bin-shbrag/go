0c45943a5a1ecec41ddd188039150fb2d464f925
bin-shbrag <hmzbnshbraq@gmail.com>
2026-03-06 00:57:20 +0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 // ===== Smart Soft DSRS Config =====
 // ملاحظة مهمة: لا يمكن إخفاء عنوان الـ API بشكل كامل في مشروع Static.
 // هنا نستخدم Base64 كـ "Obfuscation" بسيطة فقط، بينما الحماية الحقيقية تتم في Google Apps Script.
-export const API_B64 = "aHR0cHM6Ly9zY3JpcHQuZ29vZ2xlLmNvbS9tYWNyb3Mvcy9BS2Z5Y2J5SDVuX2lzY3pfdWljNFBiS2tIamVDbDl3VWtHYS03MGxuWUhObk9ndTZ6alZKaWdVcURKbDZybVR5eC1QTG55czZOQS9leGVj";
+export const API_B64 = "aHR0cHM6Ly9zY3JpcHQuZ29vZ2xlLmNvbS9tYWNyb3Mvcy9BS2Z5Y2J5NVhoNTBvSHdhd2NrZFFlaUFKNFhhUU5JZUl2djBGaXI4cC1BM2JQUGxkX2t1dmR3bEJrYmZadURUcXZjVjZUaG5xUS9leGVj";
 
 // اسم المشروع وبيانات الشركة
 export const BRAND = {
